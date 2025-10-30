9eeb199304ed08bd44ab24a48aca51009e6c8238
Rocketset <rocketsetdev@gmail.com>
2025-10-29 23:09:43 -0300

    Ajuste de imagem

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteCompression({
-      algorithm: 'gzip',
-      ext: '.gz',
-      threshold: 10240,
-      deleteOriginFile: false
-    }),
-    viteCompression({
-      algorithm: 'brotliCompress',
-      ext: '.br',
-      threshold: 10240,
-      deleteOriginFile: false
-    })
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
     open: true
   },
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          swiper: ['swiper'],
-          icons: ['lucide-react', 'react-icons']
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000,
-    assetsInlineLimit: 4096,
-    minify: 'esbuild'
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'swiper', 'aos']
+    sourcemap: true
   }
 })
